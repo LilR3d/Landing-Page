22d11fb399d8c4053b603204dcd2d8793d30fd8e
Rachel <thatbtchred@gmail.com>
2025-12-10 18:21:00 -0600

    modified my html, js, and css files

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const SUPABASE_URL = "https://ebfgufvadojmhmvlgyzn.supabase.co";
+const SUPABASE_API_KEY = 
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViZmd1ZnZhZG9qbWhtdmxneXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMTM5NDQsImV4cCI6MjA4MDc4OTk0NH0.5Rp3IC4wiJsmeXtNSBe2NESn_IIMyXsGuCKQUb98ZbM";
+
+
 async function saveData() {
   const nameValue = document.getElementById("nameInput").value;
   const emailValue = document.getElementById("emailInput").value;
@@ -8,10 +13,6 @@ async function saveData() {
     email: emailValue,
     message: messageValue,
   };
-
-  const SUPABASE_URL = "https://ebfgufvadojmhmvlgyzn.supabase.co";
-  const SUPABASE_API_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViZmd1ZnZhZG9qbWhtdmxneXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMTM5NDQsImV4cCI6MjA4MDc4OTk0NH0.5Rp3IC4wiJsmeXtNSBe2NESn_IIMyXsGuCKQUb98ZbM";
 
   try {
     const response = await fetch(`${SUPABASE_URL}/rest/v1/leads`, {
@@ -38,3 +39,42 @@ async function saveData() {
     alert("Network error. Check console for details.");
   }
 }
+
+const REST_ENDPOINT = `${SUPABASE_URL}/rest/v1/products`;
+
+async function getProducts() {
+  try {
+    const response = await fetch(REST_ENDPOINT, {
+      method: "GET",
+      headers: {
+        apikey: SUPABASE_API_KEY,
+        Authorization: `Bearer ${SUPABASE_API_KEY}`,
+      },
+    });
+
+    const products = await response.json();
+
+    const container = document.getElementById("products-list");
+
+    products.forEach((product) => {
+      const card = document.createElement("div");
+      card.className = "product-card";
+
+      card.innerHTML = `
+        <img src="${product.image_url}" alt="${product.name}" />
+        <h3>${product.name}</h3>
+        <p><strong>Price: $${product.price}</strong></p>
+        <p>${product.description}</p>
+      `;
+
+      container.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
+
+
+window.onload = function () {
+  getProducts();
+};
